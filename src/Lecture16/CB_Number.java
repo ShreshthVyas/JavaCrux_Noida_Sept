0cf9cecf2e40c7885f68a8728badf0cb59ac3db4
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2023-11-25 21:31:30 +0530

    String QPS

--- src/Lecture16/CB_Number.java
+++ src/Lecture16/CB_Number.java
@@ -4,7 +4,6 @@ public class CB_Number {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		String s = "4991";
 		boolean arr[] =  new boolean[s.length()];
 		int count  =0;
