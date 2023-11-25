3b931dca616026ddb760200700ae6397a51f1129
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2023-11-25 21:32:59 +0530

    Recursion-4

--- src/Lecture21/Coin_Permutation.java
+++ src/Lecture21/Coin_Permutation.java
@@ -9,36 +9,36 @@ public class Coin_Permutation {
 		coinchange(coin,n ,"");
 	}
 
-//	public static void coinchange(int[] coin, int n,int amnt,String ans) {
-//		// TODO Auto-generated method stub
-//		if(amnt>n) {
-//			return;
-//		}
-//		if(amnt==n) {
-//			System.out.println(ans);
-//			return;
-//		}
-//		
-//		for (int i = 0; i < coin.length; i++) {
-//			coinchange(coin,n,amnt+coin[i],ans+coin[i]);
-//		}
-//		
-//		
-//	}
-	
-	public static void coinchange(int[] coin, int n,String ans) {
+	public static void coinchange(int[] coin, int n,int amnt,String ans) {
 		// TODO Auto-generated method stub
-		if(n<0) {
+		if(amnt>n) {
 			return;
 		}
-		if(n==0) {
+		if(amnt==n) {
 			System.out.println(ans);
 			return;
 		}
 		
-		for (int i = coin.length-1; i>=0; i--) {
-			coinchange(coin,n-coin[i],ans+coin[i]);
+		for (int i = 0; i < coin.length; i++) {
+			coinchange(coin,n,amnt+coin[i],ans+coin[i]);
 		}
+		
+		
+	}
+	
+//	public static void coinchange(int[] coin, int n,String ans) {
+//		// TODO Auto-generated method stub
+//		if(n<0) {
+//			return;
+//		}
+//		if(n==0) {
+//			System.out.println(ans);
+//			return;
+//		}
+//		
+//		for (int i = coin.length-1; i>=0; i--) {
+//			coinchange(coin,n-coin[i],ans+coin[i]);
+//		}
 		
 		
 	}
