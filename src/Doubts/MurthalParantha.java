21eef4d49a634c0fd0b2f9dcb02aa5e949584aba
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2023-12-27 13:46:12 +0530

    Doubts

--- src/Doubts/MurthalParantha.java
+++ src/Doubts/MurthalParantha.java
@@ -1,10 +1,65 @@
 package Doubts;
 
+import java.util.Scanner;
+
 public class MurthalParantha {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 Scanner sc = new Scanner(System.in);
+			int paratha=sc.nextInt();
+			int n = sc.nextInt();
+			
+		       int[] chefs= new int[n];
+		       for (int i = 0; i < chefs.length; i++) {
+				chefs[i] = sc.nextInt();
+			}
+		      // minimum_time(chefs, paratha);
+		       System.out.println(minimum_time(chefs, paratha));
+		       
+			}
+			
+			public static int minimum_time(int[] chefs,int no_of_paratha ) {
+				int lo=0;
+				int hi=0;
+				int ans=0;
+				for (int i =1; i <= no_of_paratha; i++) {
+					hi+=chefs[chefs.length-1]*i;
+				}
+				while(lo<=hi) {
+					int mid=(lo+hi)/2;
+					if(is_it_possible(chefs,mid,no_of_paratha)) {
+						ans=mid;
+						hi=mid-1;
+					}else {
+						lo=mid+1;
+					}
+				}
+				return ans;
+	            }
+				public  static boolean is_it_possible(int[] chefs, int mid, int no_of_paratha) {
+				int paratha=0;
+				int initial_time=0;
+				int i=0;
+				int x=1;
+				while(i<chefs.length) {
+					if(x*chefs[i]+initial_time<=mid) {
+						initial_time+=x*chefs[i];
+						
+						paratha++;
+						x++;
+						
+					}else {
+						i++;
+						initial_time=0;
+						x=1;
+					}
+					if(paratha==no_of_paratha) {
+						return true;
+					}
+				}
+			
+				return false;
 	}
 
 }
--- src/Doubts/PainterPartition.java
+++ src/Doubts/PainterPartition.java
@@ -1,10 +1,44 @@
 package Doubts;
 
+import java.util.Scanner;
+
 public class PainterPartition {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 Scanner sc = new Scanner(System.in);
+			int k = sc.nextInt();
+			int n = sc.nextInt();
+			int arr [] = new int[n];
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
+			}
+			System.out.println(partition(arr, n, k));
+		}
+			public static int sum(int arr[], int from, int to)
+			{
+			    int total = 0;
+			    for (int i = from; i <= to; i++)
+			        total += arr[i];
+			    return total;
+			}
+			  
+			// for n boards and k partitions
+			public static int partition(int arr[], int n, int k)
+			{
+			    // base cases   
+			    if (k == 1) // one partition
+			        return sum(arr, 0, n - 1);   
+			    if (n == 1)  // one board
+			        return arr[0];
+			  
+			    int best = Integer.MAX_VALUE;
+			  
+			    for (int i = 1; i <= n; i++)
+			        best = Math.min(best, Math.max(partition(arr, i, k - 1),
+			                                sum(arr, i, n - 1)));
+			  
+			    return best;
 	}
 
 }
