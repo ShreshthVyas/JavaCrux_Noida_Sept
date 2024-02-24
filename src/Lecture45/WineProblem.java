1185a8037c754e3adc34eff84b55a109f74479f4
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2024-02-24 19:45:28 +0530

    Dp-3

--- src/Lecture45/WineProblem.java
+++ src/Lecture45/WineProblem.java
@@ -6,6 +6,7 @@ public class WineProblem {
 		// TODO Auto-generated method stub
 		int wine[] = {2,3,5,1,4};
 		System.out.println(rec(wine,0 , wine.length-1, 1));;
+		System.out.println(td(wine));
 	}
 	public static int rec(int wine[], int i, int j, int year) {
 		if(i>j) {
@@ -17,24 +18,23 @@ public class WineProblem {
 		
 		return  Math.max(s, e);
 	}
-	public int td(int wine[]) {
-		int dp[][] = new int[wine.length][wine.length];
+	public static int td(int wine[]) {
+		int[][] dp = new int[wine.length][wine.length];
 		int year = wine.length;
 		for (int i = 0; i < dp.length; i++) {
-			dp[i][i] = wine[i] *year;
+			dp[i][i] = wine[i] * year;
 		}
 		year--;
 		for (int i = 1; i < dp.length; i++) {
 			for (int j = i; j < dp.length; j++) {
-				int k = j-i;
-				int s = wine[k] * year + dp[k+1][j];
-				int e = wine[j] * year + dp[k][j-1];
-				dp[i][j] = Math.max(s, e);
+				int k = j - i;
+				int s = wine[k] * year + dp[k + 1][j];
+				int e = wine[j] * year + dp[k][j - 1];
+				dp[k][j] = Math.max(e, s);
 			}
 			year--;
 		}
-		
-		return dp[dp.length-1][dp[0].length-1];
+		return dp[0][dp[0].length - 1];
 	}
 
 }
