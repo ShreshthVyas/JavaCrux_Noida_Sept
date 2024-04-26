77f5e4dc5ca6dd02b2d7c734cc896508fe5b058a
ShreshthVyas <shreshth.vyas.2000@gmail.com>
2024-04-26 11:13:53 +0530

    Final Commit

--- src/Lecture42/No_of_island.java
+++ src/Lecture42/No_of_island.java
@@ -1,5 +1,29 @@
 package Lecture42;
 
 public class No_of_island {
+	 public int numIslands(char[][] grid) {
+	      int count =0;
+	      for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				if(grid[i][j]=='1') {
+					count++;
+					dfs(grid,i,j,grid.length,grid[0].length);
+				}
+			}
+		}
+	     return count;
+	 }
 
+	private void dfs(char[][] grid, int i, int j, int n, int m) {
+		// TODO Auto-generated method stub
+		if(i<0 || i>=n||j<0||j>=m || grid[i][j]=='0') {
+			return;
+		}
+		grid[i][j] = '0';
+		dfs(grid, i+1, j, n, m);
+		dfs(grid, i, j+1, n, m);
+		dfs(grid, i-1, j, n, m);
+		dfs(grid, i, j-1, n, m);
+	}
+	 
 }
--- src/Lecture42/rottingorange.java
+++ src/Lecture42/rottingorange.java
@@ -1,5 +1,56 @@
 package Lecture42;
 
-public class rottingorange {
+import java.util.LinkedList;
+import java.util.Queue;
 
+public class rottingorange {
+	public class pair{
+        int row;
+        int col;
+        int tm;
+        public pair(int r, int c, int t){
+            this.row =r;
+            this.col=c;
+            this.tm= t;
+        }
+    }
+    public int orangesRotting(int[][] grid) {
+        Queue<pair> q =  new LinkedList<>();
+        int visited[][] = new int[grid.length][grid[0].length];
+        int cntfresh=0;
+        for(int i= 0; i<grid.length;i++){
+            for(int j=0; j<grid[0].length;j++){
+                if(grid[i][j]==2){
+                    q.add(new pair(i,j,0));
+                    visited[i][j]=2;
+                }
+                else if(grid[i][j]==1){
+                    cntfresh++;
+                }
+            }
+        }
+        int ans=0;
+        int count =0;
+        int rowidx[]= {0,1,0,-1};
+        int colidx[]= {1,0,-1,0};
+        while(!q.isEmpty()){
+            
+            pair p = q.poll();
+            ans = Math.max(ans,p.tm);
+            for(int i =0; i<4;i++){
+                int currow= p.row + rowidx[i];
+                int curcol= p.col + colidx[i];
+                if(currow>=0 && currow<grid.length&&curcol>=0 && curcol<grid[0].length&& grid[currow][curcol]==1&&visited[currow][curcol]==0){
+                    // System.out.println(currow+" " +curcol+ " ");
+                    q.add(new pair(currow,curcol,p.tm+1));
+                    visited[currow][curcol]=2;
+                    count++;
+               }
+            }
+            
+        }
+        // System.out.println(count + " " + cntfresh);
+        // System.out.println(ans);
+        return count==cntfresh?ans:-1;
+    }
 }
